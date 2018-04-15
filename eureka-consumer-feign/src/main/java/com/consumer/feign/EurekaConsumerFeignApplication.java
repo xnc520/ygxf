63f87b9dd4a3debd9467febf73b098414e4fd4c3
薛朝选 <1276162287@qq.com>
2018-04-15 09:45:25 +0800

    解决feign不能调用问题

--- eureka-consumer-feign/src/main/java/com/consumer/feign/EurekaConsumerFeignApplication.java
+++ eureka-consumer-feign/src/main/java/com/consumer/feign/EurekaConsumerFeignApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EurekaConsumerFeignApplication {
 
+    /**
+     *
+     */
     @Autowired
     private HelloServie helloServie;
 
@@ -22,7 +26,7 @@ public class EurekaConsumerFeignApplication {
     }
 
     @RequestMapping("/hi")
-    public String hi(String name) {
+    public String hi(@RequestParam(value = "name") String name) {
         return helloServie.hi(name);
     }
 }
